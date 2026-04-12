f7385172f6cb347586af45d9413b58d20b78d57d
Neotko <sebsucmor@gmail.com>
2026-04-12 12:27:47 +0200

    fix: restore dynamic GENERATOR_IS_MULTI_CONFIG detection in FindOpenVDB

--- cmake/modules/FindOpenVDB.cmake
+++ cmake/modules/FindOpenVDB.cmake
@@ -238,8 +238,7 @@ endif()
 set(OpenVDB_LIB_COMPONENTS "")
 set(OpenVDB_DEBUG_SUFFIX "d" CACHE STRING "Suffix for the debug libraries")
 
-# get_property(_is_multi GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
-set(_is_multi FALSE)
+get_property(_is_multi GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
 
 foreach(COMPONENT ${OpenVDB_FIND_COMPONENTS})
   set(LIB_NAME ${COMPONENT})
